aa694718863fdacddcfdb233f1c284b9e3662832
Masamoto Miyata <miyata@sincere-co.com>
2020-10-01 16:13:34 +0900

    Initial Commit

---
+++ SincereIosLib/SincereIosLib.h
@@ -0,0 +1,13 @@
+//
+//  SincereIosLib.h
+//  SincereIosLib
+//
+//  Created by 宮田　雅元 on 2020/10/01.
+//  Copyright © 2020 宮田　雅元. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SincereIosLib : NSObject
+
+@end
---
+++ SincereIosLib/SincereIosLib.m
@@ -0,0 +1,13 @@
+//
+//  SincereIosLib.m
+//  SincereIosLib
+//
+//  Created by 宮田　雅元 on 2020/10/01.
+//  Copyright © 2020 宮田　雅元. All rights reserved.
+//
+
+#import "SincereIosLib.h"
+
+@implementation SincereIosLib
+
+@end
